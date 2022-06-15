974986f07f108ea2bf161e0ee9053773198037ae
SCR-athif <80390996+SCR-athif@users.noreply.github.com>
2022-06-15 07:33:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -35,3 +35,42 @@ try:
 except KeyboardInterrupt:
     print("unwanted input. Exiting...")
 
+#!/usr/bin/python3
+import os
+import time
+import pyfiglet
+import datetime
+
+import Output
+import scan
+from termcolor import colored
+
+print('-'*60)
+scanit=colored(pyfiglet.figlet_format("S c a n I T"),'cyan')
+for i in scanit:
+    print(i,end='')
+    time.sleep(.001)
+print('-'*60)
+date=datetime.datetime.now()
+print("date",date.date())
+print("time",date.time())
+print('-'*60,"\n")
+
+
+print("Choose option: \n 1. Simple Port Scan \n 2. Web Scanning \n 3. Version Scanning \n 4. OS Scanning")
+try:
+    a = int(input("Enter here: "))
+
+    if (a == 1):
+        scan.portscan()
+    elif (a == 2):
+        scan.webscan()
+    elif (a == 3):
+        scan.version()
+    elif (a == 4):
+        scan.osscanning()
+    else:
+        print("Wrong Entry")
+except KeyboardInterrupt:
+    print("unwanted input. Exiting...")
+
