6737d10a0d2e90791d06dc3ee39b067cc32ce2a5
SCR-athif <80390996+SCR-athif@users.noreply.github.com>
2022-06-16 05:18:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,38 @@
 #!/usr/bin/python3
-import os
+
 import time
 import pyfiglet
 import datetime
-
-import Output
-import scan
-from termcolor import colored
-
-print('-'*65)
-scanit=colored(pyfiglet.figlet_format("S c a n I T"),'cyan')
-for i in scanit:
-    print(i,end='')
-    time.sleep(.001)
-print('-'*60)
-date=datetime.datetime.now()
-print("date",date.date())
-print("time",date.time())
-print('-'*60,"\n")
-
-
-print("Choose option: \n 1. Simple Port Scan \n 2. Web Scanning \n 3. Version Scanning")
-try:
-    a = int(input("Enter here: "))
-
-    if (a == 1):
-        scan.portscan()
-    elif (a == 2):
-        scan.webscan()
-    elif (a == 3):
-        scan.version()
-    else:
-        print("Wrong Entry")
-except KeyboardInterrupt:
-    print("unwanted input. Exiting...")
-
-#!/usr/bin/python3
-import os
-import time
-import pyfiglet
-import datetime
-
-import Output
 import scan
 from termcolor import colored
 
 print('-'*60)
-scanit=colored(pyfiglet.figlet_format("S c a n I T"),'cyan')
+scanit = colored(pyfiglet.figlet_format("S c a n I T"), 'cyan')
 for i in scanit:
-    print(i,end='')
+    print(i, end='')
     time.sleep(.001)
+
 print('-'*60)
-date=datetime.datetime.now()
-print("date",date.date())
-print("time",date.time())
-print('-'*60,"\n")
+date = datetime.datetime.now()
+print("date", date.date())
+print("time", date.time())
+print('-'*60, "\n")
 
-
-print("Choose option: \n 1. Simple Port Scan \n 2. Web Scanning \n 3. Version Scanning \n 4. OS Scanning")
+print("Choose option: \n 1. Simple Port Scan \n 2. Web Scanning \n 3. Version Scanning \n 4. OS Scanning \n 5. Total Hosts finding")
 try:
     a = int(input("Enter here: "))
-
-    if (a == 1):
+    if a == 1:
         scan.portscan()
-    elif (a == 2):
+    elif a == 2:
         scan.webscan()
-    elif (a == 3):
+    elif a == 3:
         scan.version()
-    elif (a == 4):
+    elif a == 4:
         scan.osscanning()
+    elif a == 5:
+        scan.Total()
     else:
         print("Wrong Entry")
+
 except KeyboardInterrupt:
     print("unwanted input. Exiting...")
-
