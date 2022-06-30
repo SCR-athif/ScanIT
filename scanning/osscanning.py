8bc442e787f8aa867a9d62e6a8afe7837c2f58c2
SCR-athif <solocoderider@gmail.com>
2022-06-30 06:45:39 -0400

    perferct

--- scanning/osscanning.py
+++ scanning/osscanning.py
@@ -1,75 +1,79 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
-#importing Module 
-import Output
-from scapy.layers.inet import IP, ICMP, sr1
-from os import system
-from package import map
+import optparse, Output
+import scapy.all as scapy
+from scapy.layers.inet import IP, ICMP
 from termcolor import colored
+from os import system
+
 try:
     system('clear')
-    # program to find OS in a network
-    target = input("Enter the Ip address or Host: ")
-    nm = map.nmap.PortScanner()
-    if '/24' in target:
-        nm.scan(hosts=target, arguments='-n -sP')
-        hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+
+
+    def createPacket(ip):
+        arp_request = scapy.ARP(pdst=ip)  # create a ARP request object by scapy
+        broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  # We have set the destination
+        arp_request_broadcast = broadcast / arp_request
+        return (arp_request_broadcast)
+
+
+    def transmitPacket(packet):
+        success_list, failure_list = scapy.srp(packet, timeout=1)
+        return success_list
+
+
+    def getOS(ip_addr):
+        ttl_values = {32: "Windows", 60: "MAC OS", 64: "Linux", 128: "Windows", 255: "Linux 2.4 Kernal"}
+        ans = scapy.sr1(IP(dst=str(ip_addr)) / ICMP(), timeout=1, verbose=0)
+        if ans:
+            if ans.ttl in ttl_values:
+                return ttl_values.get(ans.ttl)
+            else:
+                return "could not figure the OS version"
+        else:
+            return "Packets could not send successfully"
+
+
+    def parseResponse(success_list):
+        targets = []
+        for success in success_list:
+            entry = {'ip': success[1].psrc, 'mac': success[1].hwsrc}
+            targets.append(entry)
+        return targets
+
+
+    def print_analysis(element_entries):
         Output.newline()
+        h = colored("\nIP\t\t\tMAC Address\t\t\tOPERATING SYSTEM", 'red')
+        print(h)
+        Output.osdata(h)
+        print("." * 100)
+        for element in entries:
+            a = colored("\n" + element["ip"] + "\t\t" + element['mac'] + "\t\t" + getOS(element["ip"]) + "\n", 'white')
+            Output.osdata(a)
+            print(a)
 
-        for host in hosts_list:
-            t=host[0]
-            os = ''
-            pack = IP(dst=t) / ICMP()
-            resp = sr1(pack, timeout=3)
-            if resp:
-                if IP in resp:
-                    ttl = resp.getlayer(IP).ttl
-                    if ttl <= 64:
-                        os = 'Linux'
-                    elif ttl > 64:
-                        os = 'Windows'
-                    else:
-                        print('Not Found')
-                    print('-'*100)
-                    a = colored(f'\n\n*{t} : {os}* Operating System is Detected \n\n','green')
-                    Output.osdata(a)
-                    print(a)
-                    print('-'*100)
-                    print('\n\n')
-        back = input("Do you want scan again (y/n): ")
+        back = input("\n\nDo you want scan again (Y/N): ")
         if back == 'y' or back == 'Y':
             system('./scanning/osscanning.py')
         else:
             system('./scanning/main.py')
 
-    else:
 
-        os = ''
-        pack = IP(dst=target) / ICMP()
-        resp = sr1(pack, timeout=3)
-        if resp:
-            if IP in resp:
-                ttl = resp.getlayer(IP).ttl
-                if ttl <= 64:
-                    print(ttl)
-                    os = 'Linux'
-                elif ttl > 64:
-                    os = 'Windows'
-                else:
-                    print('Not Found')
-                a = colored(f'\n\n*{target} : {os}* Operating System is Detected \n\n','green')
-                Output.newline()
-                Output.osdata(a)
-                print(a)
-        back = input("Do you want scan again (y/n): ")
-        if back == 'y' or back == 'Y':
-            system('./scanning/osscanning.py')
-        else:
-            system('./scanning/main.py')
+    options = input("Enter IP or Host (eg:- 192.168.1.1) : ")
+
+    if options is not None:
+        broadcast_packets = createPacket(options)
+        success_packets = transmitPacket(broadcast_packets)
+        entries = parseResponse(success_packets)
+        print_analysis(entries)
+
 except KeyboardInterrupt:
     print("\n\n")
     back = input("Do you want to EXIT (y/n): ")
     if back == 'n' or back == 'N':
         system('./scanning/osscanning.py')
+
     else:
         system('./scanning/main.py')
+
